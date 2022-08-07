91a0dd0cb798664efa5cc6bd237e4b509ca874ac
Dee <dgrowette@gmail.com>
2022-08-07 16:17:22 -0700

    added assets folder and put some images in it

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,17 +4,15 @@ export default function Menu({ menuOpen, setMenuOpen }) {
   return (
     <div className={'menu ' + (menuOpen && 'active')}>
         <ul>
-            <li>
+            <li onClick={()=>setMenuOpen(false)}>
                 <a href="#hero">Home</a>
             </li>
-            <li>
+            <li onClick={()=>setMenuOpen(false)}>
                 <a href="#catalog">Catalog</a>
             </li>
-            <li>
-                <a href="#Footer">Support</a>
+            <li onClick={()=>setMenuOpen(false)}>
+                <a href="#footer">Support</a>
             </li>
-            
-           
         </ul>
     </div>
   )
