5f245fea7a39ea6576f50f11e6552b663de8f409
Dee <dgrowette@gmail.com>
2022-08-07 22:14:57 -0700

    added icon packages and fixed some sizing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 import "./navbar.scss";
-import QueueIcon from '@mui/icons-material/Queue';
+import QueueIcon from "@mui/icons-material/Queue";
 
 export default function Navbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"navbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#hero" className="logo">CTV <span>Radio</span></a>
+          <a href="#hero" className="logo">
+            CTV <span>Radio</span>
+          </a>
           <div className="itemContainer">
             <QueueIcon className="icon" />
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfv8BOmk0Vfwtot5YTauNWYUWdbPkqqxna4D5atp3Q8ff-nUA/viewform">Submit A Song</a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfv8BOmk0Vfwtot5YTauNWYUWdbPkqqxna4D5atp3Q8ff-nUA/viewform">
+              Submit A Song
+            </a>
           </div>
         </div>
         <div className="right">
