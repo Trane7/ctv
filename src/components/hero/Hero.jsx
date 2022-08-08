0ff0abacc6d1ba299d62b9e25d9991d6051500d4
Dee <dgrowette@gmail.com>
2022-08-07 19:40:58 -0700

    added tiktok video in hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import './hero.scss';
+import { TikTok } from 'react-tiktok';
 
 export default function Hero() {
   return (
@@ -10,6 +11,9 @@ export default function Hero() {
         </div>
         <div className="right">
           <div className="wrapper">
+            <div className='tiktok'>
+              <TikTok url='https://www.tiktok.com/@ctvradio/video/7098337893336288554?is_from_webapp=1&sender_device=pc&web_id=7102566022927025706' />
+            </div>
           <a href="https://www.tiktok.com/@ctvradio" >
             <button>Tune In</button>
             </a>            
